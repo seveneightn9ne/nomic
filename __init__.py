310594e7e2ee8e0426fbc4b3298c23f184924600
Jess Kenney <seveneightn9ne@gmail.com>
2014-09-09 17:24:18 -0400

    minimal working app

---
+++ __init__.py
@@ -0,0 +1,77 @@
+from flask import Flask, redirect, request, render_template, url_for
+from flask.ext.mongoengine import MongoEngine
+
+# from nomic.views import proposals, votes
+
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {'DB': "nomic_db"}
+app.config["SECRET_KEY"] = "super-secret"
+
+db = MongoEngine(app)
+
+from models import Proposal, Vote
+
+# def register_blueprints(app):
+#     # Prevents circular imports
+#     from nomic.views import proposals, votes
+#     app.register_blueprint(proposals)
+#     app.register_blueprint(votes)
+
+# register_blueprints(app)
+
+# app.register_blueprint(proposals)
+# app.register_blueprint(votes)
+
+def get_sanitized_proposals():
+    proposals = Proposal.objects(archived=False)
+    for proposal in proposals:
+        if not proposal.votes_revealed:
+            for vote in proposal.votes:
+                vote.vote = "[hidden]"
+    return proposals
+
+@app.route('/', methods=['GET', 'POST'])
+def list():
+    if request.method == 'GET':
+        return render_template('proposals/list.html', proposals=get_sanitized_proposals())
+    else: # POST
+        proposal = Proposal.from_form_data(request.form)
+        proposal.save()
+
+        return render_template('proposals/list.html', proposals=get_sanitized_proposals())
+
+@app.route('/vote/<proposal_id>', methods=['POST'])
+def add_vote(proposal_id):
+    vote = Vote(name=request.form['name'], vote=request.form['vote'])
+    proposal = Proposal.objects.get_or_404(id=proposal_id)
+
+    existing_votes = filter(lambda v: v.name == vote.name, proposal.votes)
+    if len(existing_votes) > 0:
+        existing_vote = existing_votes[0]
+        existing_vote.created_at = vote.created_at
+        existing_vote.vote = vote.vote
+    else:
+        proposal.votes.append(vote)
+
+    proposal.save()
+
+    return redirect(url_for('list'))
+
+@app.route('/archive/<proposal_id>')
+def archive(proposal_id):
+    proposal = Proposal.objects.get(id=proposal_id)
+    proposal.archived = True
+    proposal.save()
+
+    return redirect(url_for('list'))
+
+@app.route('/reveal/<proposal_id>')
+def reveal(proposal_id):
+    proposal = Proposal.objects.get(id=proposal_id)
+    proposal.votes_revealed = True
+    proposal.save()
+
+    return redirect(url_for('list'))
+
+if __name__ == "__main__":
+    app.run()
---
+++ models.py
@@ -0,0 +1,28 @@
+import datetime
+from flask import url_for
+from nomic import db
+
+class Proposal(db.Document):
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    created_by = db.StringField(max_length=255, required=False)
+    number = db.StringField(max_length=255, required=True)
+    votes_revealed = db.BooleanField(default=False)
+    archived = db.BooleanField(default=False)
+    votes = db.ListField(db.EmbeddedDocumentField('Vote'))
+
+    def __unicode__(self):
+        return self.number
+
+    def get_absolute_url(self):
+        return url_for('proposal', kwargs={"number": self.number})
+
+    meta = {
+        'allow_inheritance': True,
+        'indexes': ['-created_at', 'number'],
+        'ordering': ['-created_at']
+    }
+
+class Vote(db.EmbeddedDocument):
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    name = db.StringField(max_length=255, required=True)
+    vote = db.StringField(max_length=255, required=True)
