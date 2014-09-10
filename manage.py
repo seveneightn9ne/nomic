c67a0d790170e144282f568d75a98f7824f6bf11
Jess Kenney <jessk@jesskenney.com>
2014-09-09 22:44:38 -0400

    Use url_for instead of real URLs

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ manager = Manager(app)
 
 # Turn on debugger by default and reloader
 manager.add_command("runserver", Server(
-    use_debugger = False,
+    use_debugger = True,
     use_reloader = True,
     host = '0.0.0.0',
     port=8789,
